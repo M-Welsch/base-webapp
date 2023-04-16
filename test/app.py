14b2b8e6dd000b617e6588ddbf097d6147999bbb
jackyscript <8930712+jackyscript@users.noreply.github.com>
2023-04-16 23:52:34 +0200

    Minor refactor

--- test/app.py
+++ test/app.py
@@ -1,17 +1,18 @@
 import os
-from flask import Flask, url_for, send_file
-
+from flask import Flask, send_file
 from views.main import bp as main_bp
 
 app = Flask(__name__, static_folder="static")
 
 app.register_blueprint(main_bp)
-js_source_path='CUSTOM_STATIC_PATH'
+webapp_static_path_key="WEBAPP_STATIC_PATH"
+webapp_static_path_fragment="/webapp/static"
 # set the path where you store your assets - it can be outside of the root dir
-app.config[js_source_path] = os.path.abspath(os.path.join(os.path.dirname(__file__), '../webapp/static'))
+app.config[webapp_static_path_key] = os.path.abspath(os.path.join(os.path.dirname(__file__), ".." + webapp_static_path_fragment))
 
 # have a route listening for requests
-@app.route('/webapp/static/<path:filename>')
-def send_media(filename):
-    path = os.path.join(app.config[js_source_path], filename)
+@app.route(webapp_static_path_fragment +  "/<path:filename>")
+def send_static_asset(filename):
+    path = os.path.join(app.config[webapp_static_path_key
+], filename)
     return send_file(path)
