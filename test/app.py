5aa75363f482f0ac06aa2311c0ef119a82492276
jackyscript <8930712+jackyscript@users.noreply.github.com>
2023-04-24 22:07:32 +0200

    Refactor

--- test/app.py
+++ test/app.py
@@ -6,12 +6,12 @@ app = Flask(__name__, static_folder="static")
 
 app.register_blueprint(main_bp)
 webapp_static_path_key="WEBAPP_STATIC_PATH"
-webapp_static_path_fragment="/webapp/static"
+webapp_static_path_fragment="webapp/static"
 # set the path where you store your assets - it can be outside of the root dir
-app.config[webapp_static_path_key] = Path(__file__).resolve().parents[1] / webapp_static_path_fragment[1:]
+app.config[webapp_static_path_key] = Path(__file__).resolve().parents[1] / webapp_static_path_fragment
 
 # have a route listening for requests
-@app.route(f"{webapp_static_path_fragment}/<path:filename>")
+@app.route(f"/{webapp_static_path_fragment}/<path:filename>")
 def send_static_asset(filename):
     path = app.config[webapp_static_path_key] / filename
     return send_file(path)
