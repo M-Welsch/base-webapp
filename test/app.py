a15f1766c191f66145e66a784d33fdd98ec898d0
jackyscript <8930712+jackyscript@users.noreply.github.com>
2023-04-24 22:05:06 +0200

    Replace os path with pathlib

--- test/app.py
+++ test/app.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 from flask import Flask, send_file
 from views.main import bp as main_bp
 
@@ -8,11 +8,10 @@ app.register_blueprint(main_bp)
 webapp_static_path_key="WEBAPP_STATIC_PATH"
 webapp_static_path_fragment="/webapp/static"
 # set the path where you store your assets - it can be outside of the root dir
-app.config[webapp_static_path_key] = os.path.abspath(os.path.join(os.path.dirname(__file__), ".." + webapp_static_path_fragment))
+app.config[webapp_static_path_key] = Path(__file__).resolve().parents[1] / webapp_static_path_fragment[1:]
 
 # have a route listening for requests
-@app.route(webapp_static_path_fragment +  "/<path:filename>")
+@app.route(f"{webapp_static_path_fragment}/<path:filename>")
 def send_static_asset(filename):
-    path = os.path.join(app.config[webapp_static_path_key
-], filename)
+    path = app.config[webapp_static_path_key] / filename
     return send_file(path)
