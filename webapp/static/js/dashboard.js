b73c17bcba7e7ef13808792ff0ea0b675ff0bba3
jackyscript <8930712+jackyscript@users.noreply.github.com>
2023-06-10 18:23:07 +0200

    Replace innerHtml with textContent

--- webapp/static/js/dashboard.js
+++ webapp/static/js/dashboard.js
@@ -7,14 +7,14 @@ import { registerButtons } from "./modules/dashboard/backup/components/buttons.j
 
 export function displayDashboardData( current_status ) {
     setDiscUsage(current_status["diagnose"]["Backup-HDD verfügbar"], current_status["backup_hdd_usage"]);
-    document.getElementById("log-view").innerHTML = current_status["log_tail"].join("\n");
+    document.getElementById("log-view").textContent = current_status["log_tail"].join("\n");
     if (~~current_status["recent_warnings_count"] > 0) {
         document.getElementById("warning-banner").style.display = "flex";
     } else {
         document.getElementById("warning-banner").style.display = "none";
     }
-    document.getElementById("banner-warning-count").innerHTML = current_status["recent_warnings_count"];
-    document.getElementById("next-backup-due-text").innerHTML = current_status["next_backup_due"];
+    document.getElementById("banner-warning-count").textContent = current_status["recent_warnings_count"];
+    document.getElementById("next-backup-due-text").textContent = current_status["next_backup_due"];
 
     for (const [status, status_element] of Object.entries(status_types)) {
         setStatus(current_status, status, status_element);
@@ -22,17 +22,19 @@ export function displayDashboardData( current_status ) {
 }
 
 function setDiscUsage(availability, percentage) {
-    function setElements(bgColor, display1, display2) {
-        document.getElementById("hdd-space-free").style.backgroundColor = bgColor;
-        document.getElementById("backup-hdd-usage-text").style.display = display1;
-        document.getElementById("backup-hdd-not-available-text").style.display = display2;
-    }
 
-    document.getElementById("backup-hdd-usage-percentage").innerHTML = ~~percentage;
+    document.getElementById("backup-hdd-usage-percentage").textContent = ~~percentage;
     document.getElementById("hdd-space-used").style.width = percentage + "%";
     if ( availability == "available" ) {
         setElements("#333", "block", "none");
     } else {
         setElements("gray", "none", "block");
     }
+    
+    function setElements(bgColor, display1, display2) {
+        document.getElementById("hdd-space-free").style.backgroundColor = bgColor;
+        document.getElementById("backup-hdd-usage-text").style.display = display1;
+        document.getElementById("backup-hdd-not-available-text").style.display = display2;
+    }
+
 }
