7435cb8fbbdadd9078cdc62ac949a24a33127c78
jackyscript <8930712+jackyscript@users.noreply.github.com>
2023-06-18 00:31:47 +0200

    Apply brightness conversion

--- webapp/static/js/modules/message/json/JsonMessenger.js
+++ webapp/static/js/modules/message/json/JsonMessenger.js
@@ -56,6 +56,7 @@ export function buttonSignal(messageCode) {
 }
 
 export function setBrightness(brightness) {
+    brightness = typeof brightness === "string" ? Number(brightness) : brightness;
     let message = buildMessage("display_brightness", brightness);
     sendMessage(message);
 }
