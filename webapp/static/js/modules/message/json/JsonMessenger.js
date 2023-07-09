23995af96b19914352907d89c0feefc428be47fd
jackyscript <8930712+jackyscript@users.noreply.github.com>
2023-07-09 09:36:51 +0200

    Restrict private function

--- webapp/static/js/modules/message/json/JsonMessenger.js
+++ webapp/static/js/modules/message/json/JsonMessenger.js
@@ -1,7 +1,7 @@
 //const BASE_ADDRESS = "ws://192.168.178.39:8453"
 var BASE_ADDRESS = "ws://127.0.0.1:8453"
 
-export function sendMessageToBcu(
+function sendMessageToBcu(
     payload,
     callback = function () { },
     error_callback = function (error) { console.log(error); alert('[close] Connection died'); }
